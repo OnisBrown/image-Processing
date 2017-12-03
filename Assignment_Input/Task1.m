7ade4b861c79daecc06034ec221c16df884bc444
Onis.Brown <Onis.Brown@hotmail.co.uk>
2017-12-03 18:46:08 +0000

    completed bilinear and nearest neighbour

--- Assignment_Input/Task1.m
+++ Assignment_Input/Task1.m
@@ -20,13 +20,31 @@ cSize = size(Igray);
 ratioW = newSize(1)/cSize(1);
 ratioH = newSize(2)/cSize(2);
 newImage = zeros(1668,1836);
+IgrayNear = zeros(1668,1836);
+IgrayBi = zeros(1668,1836);
 
 %nearest neighbour
-for i = 1:ratioH
-	for j = 1:ratioW
+for i = 1:newSize(2)
+	for j = 1:newSize(1)
 		mR = i/ratioH;
 		mC = j/ratioW;
-		newImage(i,j) = Igray(mR,mC);
+		IgrayNear(i,j) = Igray(mC,mR);
 	end
 end	
 			
+%bilinear 
+
+for i = 1:newSize(2)
+	for j = 1:newSize(1)
+		temp1 = 0;
+		temp2 = 0;
+		temp3 = 0;
+		mR = i/ratioH;
+		mC = j/ratioW;
+		temp1 = (Igray(mC,mR)+Igray(mC,mR+1))/2; % interpolation along x axis
+		temp2 = (Igray(mC,mR)+Igray(mC+1,mR))/2; % interpolation along y axis
+		temp3 = (temp1 + temp2)/2
+		IgrayBi(i,j) = temp3/2;
+		%new pixel is given by 
+	end
+end	
