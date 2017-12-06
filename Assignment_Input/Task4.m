1f8dd7c6d64a833bed39702e81071e52c3211bd2
OnisBrown <onis.Brown@hotmail.co.uk>
2017-12-06 08:42:47 +0000

    more task 4

--- Assignment_Input/Task4.m
+++ Assignment_Input/Task4.m
@@ -21,20 +21,37 @@ figure;
 imshow(ISize);
 title('Starfish image resized');
 
+IPW(ISize);
+Imean = mean(ISize(:));
+for i = 1:size(ISize(:))
+        x = ISize(i);
+        if ((Imena*0.9)<x)&&(x<(Imean*1.1) % Values within 10% of the  mean are set to white
+            x = 0;
+            IPW(i) = x;
+        end
+end
+figure;
+imshow(IPW);
+title('Starfish image piecewise transformed');
+
+
 Ibi = imcomplement(ISize);
 Ibi = imbinarize(Ibi);
 figure;
 imshow(Ibi);
 title('Starfish image Shapes defined');
 
-Line = bwboundaries(Ibi);
-ILine = Ibi;
-for i = 1:size(Line)
-    j = Line{i};
-    plot(j(:,2),j(:,1),'w','LineWidth', 2);
-end
-figure;
-imshow(ILine);
-title('Starfish image with all shapes outlined');
+
+
+
+% Line = bwboundaries(Ibi);
+% ILine = Ibi;
+% for i = 1:size(Line)
+    % j = Line{i};
+    % plot(j(:,2),j(:,1),'w','LineWidth', 2);
+% end
+% figure;
+% imshow(ILine);
+% title('Starfish image with all shapes outlined');
 
 
